471f90fe69ccdc74afbd09aa1682cdd91e99a5f1
fearlesshandlee <vanshvishwakarma233@gmail.com>
2025-03-04 00:36:11 +0530

    Update ChatHeader.jsx

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -14,7 +14,7 @@ const ChatHeader = () => {
           <div className="flex items-center">
   <div className="relative w-12 h-12">
     <img 
-      src={{selectedUser.profilePic || "/avatar.png"} 
+      src={selectedUser.profilePic || "/avatar.png"} 
       alt={"User Avatar"} 
       className="w-full h-full object-cover rounded-full border-2 border-gray-300 shadow-md"
     />
