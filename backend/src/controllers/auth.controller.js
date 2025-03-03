e629bf75a400d8264961412776e28b2420021e25
fearlesshandlee <vanshvishwakarma233@gmail.com>
2025-03-03 23:55:39 +0530

    profile-pic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -89,29 +89,28 @@ export const logout = (req ,res) =>{
     }
 };
 
-export const updateProfile = async (req,res) =>{
-try {
-    const{profilePic} = req.body;
-    const userId = req.user._id;
-    console.log("Received profilePic:", profilePic);
-
-
-    if(!profilePic){
-        return res.status(400).json({message:"profilePic is required"});
-
+export const updateProfile = async (req, res) => {
+    try {
+      const { profilePic } = req.body;
+      const userId = req.user._id;
+  
+      if (!profilePic) {
+        return res.status(400).json({ message: "Profile pic is required" });
+      }
+  
+      const uploadResponse = await cloudinary.uploader.upload(profilePic);
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { profilePic: uploadResponse.secure_url },
+        { new: true }
+      );
+  
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      console.log("error in update profile:", error);
+      res.status(500).json({ message: "Internal server error" });
     }
-
-    const uploadResponse = await cloudinary.uploader.upload(profilePic)
-    const updatedUser = await User.findByIdAndUpdate(userId , {profilePic : uploadResponse.secure_url}, {new:true});
-
-    res.status(200).json(updatedUser);
-
-} catch (error) {
-    console.log("error in the update route ", error);
-    res.status(500).json({message:"internal error "});
-    
-}
-}
+  };
 
 export const checkAuth = async (req, res) => {
     try {
