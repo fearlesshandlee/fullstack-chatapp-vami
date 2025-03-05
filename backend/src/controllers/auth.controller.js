997f17d577c4a67595d2827510f7793f2519a505
fearlesshandlee <vanshvishwakarma233@gmail.com>
2025-03-05 17:41:01 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -58,11 +58,9 @@ export const signup = async (req ,res) =>{
 };
 
 export const login = async(req, res) =>{
- const {identifier , password } = req.body;
+ const {email , password } = req.body;
  try {
-    const user = await User.findOne({
-        $or: [{ email: identifier }, { userName: identifier }]
-      });
+    const user = await User.findOne({email});
       
 
     if(!user){
