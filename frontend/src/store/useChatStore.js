b3b24f77245a7d971580320a14bc6e3a42603c97
fearlesshandlee <vanshvishwakarma233@gmail.com>
2025-03-17 23:12:59 +0530

    Update useChatStore.js

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -55,33 +55,24 @@ export const useChatStore = create((set,get) =>({
     
         const socket = useAuthStore.getState().socket;
     
-        socket.on("newMessage", (newMessage) => {
-          const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
-          if (!isMessageSentFromSelectedUser) return;
-    
-          set({
-            messages: [...get().messages, newMessage],
-          });
-        });
-      },
-
-
-
-      subscribeToMessages: () => {
-        const { selectedUser } = get();
-        if (!selectedUser) return;
-    
-        const socket = useAuthStore.getState().socket;
+        if (!socket) {
+            console.error("Socket is not connected yet! Cannot subscribe to messages.");
+            return;
+        }
     
         socket.on("newMessage", (newMessage) => {
-          const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
-          if (!isMessageSentFromSelectedUser) return;
+            const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+            if (!isMessageSentFromSelectedUser) return;
     
-          set({
-            messages: [...get().messages, newMessage],
-          });
+            set({
+                messages: [...get().messages, newMessage],
+            });
         });
-      },
+    },
+    
+
+
+    
     
       
       unsubscribeFromMessages: () => {
